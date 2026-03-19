aedf055b649b456004ab4fac0c7620b98f271d10
blcklzrd01-art <blcklzrd01@gmail.com>
2026-03-19 13:51:31 +1100

    Update LevelAuthors.js

--- js/components/List/LevelAuthors.js
+++ js/components/List/LevelAuthors.js
@@ -44,7 +44,7 @@ export default {
                     <span>{{ verifier }}</span>
                 </p>
             </template>
-            <div class="type-title-sm">Publisher</div>
+            <div class="type-title-sm">Game</div>
             <p class="type-body">
                 <span>{{ author }}</span>
             </p>
